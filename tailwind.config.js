739fafed6ee5682721cc1693439d24aaefe7a01a
unknown <cameron.erwin@hotmail.com>
2023-01-07 11:42:48 -0500

    Update tailwind version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,12 @@ module.exports = {
   content: [
     './src/**/*.{html,ts}',
   ],
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       blur: {
         xs: '2px',
       }
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
