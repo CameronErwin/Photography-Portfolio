4d066d108dc3d7b8f4a86949e5593dca32357b4c
unknown <cameron.erwin@hotmail.com>
2023-01-14 10:32:07 -0500

    Remove "About" page from header for now

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -11,9 +11,6 @@ export class HeaderComponent implements OnInit {
   public menuItems: any[] = [{
     title: 'Gallery',
     path: 'gallery',
-  }, {
-    title: 'About',
-    path: 'about',
   }];
 
   constructor() { }
