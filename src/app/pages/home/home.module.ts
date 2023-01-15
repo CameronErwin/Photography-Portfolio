a3eeb64ac875555a1c9287e40c3812102b31169d
unknown <cameron.erwin@hotmail.com>
2023-01-15 16:11:52 -0500

    Home: Add missing matdialog module for expanding

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
+    MatDialogModule,
   ],
 })
 export class HomeModule { }
