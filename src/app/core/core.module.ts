c83415150beceb4406ba81e57be47cd6ada4991f
unknown <cameron.erwin@hotmail.com>
2023-02-22 17:07:12 -0500

    Add new "Side Menu" component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    SideMenuComponent,
   ],
   imports: [
     CommonModule,
